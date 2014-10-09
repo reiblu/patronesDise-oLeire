59d1bf3080df7647b3413f15cc23a2d7119b6e77
Leire <leirehe@gmail.com>
2014-10-09 10:05:20 +0200

    Añadidos estados

---
+++ src/main/java/conexion/Preparado.java
@@ -0,0 +1,41 @@
+package conexion;
+
+public class Preparado extends Estado {
+
+    @Override
+    public void abrir(Conexion conection) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void cerrar(Conexion conection) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void parar(Conexion conection) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void iniciar(Conexion conection) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void enviar(Conexion conection, String mensaje) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void recibir(Conexion conection, int respuesta) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
