72bfce22863e977ea0875e948d42b038ecec803b
Leire <leirehe@gmail.com>
2014-10-14 12:28:39 +0200

    Arreglado text mayusculas

--- src/main/java/editor/Texto.java
+++ src/main/java/editor/Texto.java
@@ -4,28 +4,40 @@ public class Texto extends Composite {
 
     @Override
     public void add(Componente componente) {
-        if(!componente.isComposite()){
+        if (!componente.isComposite()) {
             throw new UnsupportedOperationException("Acción no permitida... ");
         } else {
             compuestos.add(componente);
         }
-        
+
     }
 
     @Override
-    public String dibujar(boolean b) {
-        // TODO Auto-generated method stub
-        return null;
+    public String dibujar(boolean mayuscula) {
+        StringBuilder temporal = new StringBuilder();
+        if (!mayuscula) {
+            for (Componente componente : compuestos) {
+                temporal.append(componente.dibujar(false));
+            }
+        } else {
+            for (Componente componente : compuestos) {
+                temporal.append((componente.dibujar(true)));
+            }
+        }
+
+        temporal.append("---o---\n");
+        String resultado = temporal.toString();
+        return resultado;
     }
 
     @Override
     public void remove(Componente componente) {
-        if(!componente.isComposite()){
+        if (!componente.isComposite()) {
             throw new UnsupportedOperationException("Acción no permitida... ");
         } else {
             compuestos.remove(componente);
         }
-        
+
     }
 
 }
